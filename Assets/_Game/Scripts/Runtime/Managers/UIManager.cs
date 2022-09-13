bdb02c991863be8cf4bf782e90e5418460a96bea
Pranta Protik <pranta.cseku@gmail.com>
2022-09-13 22:40:44 +0600

    UIManager UI load bug fix

--- Assets/_Game/Scripts/Runtime/Managers/UIManager.cs
+++ Assets/_Game/Scripts/Runtime/Managers/UIManager.cs
@@ -10,12 +10,13 @@ namespace _Game.Managers
         private void Start()
         {
 #if UNITY_EDITOR
-            if (SceneLoadManager.Instance) return;
-            
-            var sceneLoadManagerGO = new GameObject("SceneLoadManager");
-            sceneLoadManagerGO.SetActive(false);
-            sceneLoadManagerGO.AddComponent<SceneLoadManager>().EnableTestMode();
-            sceneLoadManagerGO.SetActive(true);
+            if (!SceneLoadManager.Instance)
+            {
+                var sceneLoadManagerGO = new GameObject("SceneLoadManager");
+                sceneLoadManagerGO.SetActive(false);
+                sceneLoadManagerGO.AddComponent<SceneLoadManager>().EnableTestMode();
+                sceneLoadManagerGO.SetActive(true);   
+            }
 #endif
             SceneLoadManager.Instance.LoadUIScene();
         }
