29022fe95d2d8ee051071d5ba8ecd49665e2a88a
Pranta Protik <pranta.cseku@gmail.com>
2022-10-03 16:31:58 +0600

    Material property override scripts added

---
+++ Assets/_Tools/Scripts/Editor/Helpers/MaterialPropertyOverrideEditor.cs
@@ -0,0 +1,250 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace _Tools.Helpers
+{
+    [CustomEditor(typeof(MaterialPropertyOverride))]
+    [CanEditMultipleObjects]
+    public class MaterialPropertyOverrideEditor : Editor
+    {
+        private struct ShaderPropertyInfo
+        {
+            public string property;
+            public ShaderPropertyType type;
+            public string description;
+            public float rangeMin;
+            public float rangeMax;
+
+        }
+
+        private static readonly Dictionary<int, List<ShaderPropertyInfo>> _shaderPropertyDict = new();
+
+        private static List<ShaderPropertyInfo> GetShaderProperties(Shader shader)
+        {
+            if (_shaderPropertyDict.ContainsKey(shader.GetInstanceID())) return _shaderPropertyDict[shader.GetInstanceID()];
+
+            var propertyInfo = new List<ShaderPropertyInfo>();
+            var propertyCount = ShaderUtil.GetPropertyCount(shader);
+
+            for (var i = 0; i < propertyCount; i++)
+            {
+                var shaderProperty = new ShaderPropertyInfo
+                {
+                    property = ShaderUtil.GetPropertyName(shader, i),
+                    type = (ShaderPropertyType)ShaderUtil.GetPropertyType(shader, i),
+                    description = ShaderUtil.GetPropertyDescription(shader, i)
+                };
+                if (shaderProperty.type == ShaderPropertyType.Range)
+                {
+                    shaderProperty.rangeMin = ShaderUtil.GetRangeLimits(shader, i, 1);
+                    shaderProperty.rangeMax = ShaderUtil.GetRangeLimits(shader, i, 2);
+                }
+
+                propertyInfo.Add(shaderProperty);
+            }
+
+            return _shaderPropertyDict[shader.GetInstanceID()] = propertyInfo;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            var materialPropertyOverride = target as MaterialPropertyOverride;
+            if (materialPropertyOverride == null) return;
+
+            EditorGUILayout.Space();
+
+            var headerStyle = new GUIStyle("ShurikenModuleTitle")
+            {
+                fixedHeight = 20f,
+                contentOffset = new Vector2(5f, -2f),
+                font = EditorStyles.boldFont
+            };
+
+            var renderedHeader = GUILayoutUtility.GetRect(20f, 22f, headerStyle);
+            GUI.Box(renderedHeader, "Affected Renderers", headerStyle);
+
+            EditorGUI.indentLevel += 1;
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_renderer"), true);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                materialPropertyOverride.ClearMaterialProperties();
+
+                serializedObject.ApplyModifiedProperties();
+
+                materialPropertyOverride.ClearMaterialProperties();
+                materialPropertyOverride.ApplyMaterialProperties();
+            }
+
+            EditorGUI.indentLevel -= 1;
+
+            EditorGUILayout.Space();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Find Renderer in GameObject", "Button"))
+            {
+                Undo.RecordObject(materialPropertyOverride, "Find");
+                materialPropertyOverride.FindRenderer();
+            }
+
+            GUILayout.EndHorizontal();
+
+            if (materialPropertyOverride.Renderer == null)
+            {
+                EditorGUILayout.HelpBox("No renderers affected!", MessageType.Error);
+                return;
+            }
+
+            var materials = new List<Material>();
+
+            foreach (var sharedMaterial in materialPropertyOverride.Renderer.sharedMaterials)
+            {
+                if (sharedMaterial == null) continue;
+                if (materials.Contains(sharedMaterial)) continue;
+                materials.Add(sharedMaterial);
+            }
+
+            foreach (var mat in materials)
+            {
+                if (materialPropertyOverride.MaterialOverrides.Find(x => x.material == mat) != null) continue;
+
+                var materialOverride = new MaterialPropertyOverride.MaterialOverride
+                {
+                    material = mat
+                };
+                materialPropertyOverride.MaterialOverrides.Add(materialOverride);
+            }
+
+            materialPropertyOverride.MaterialOverrides.RemoveAll(x => !materials.Contains(x.material));
+
+            var hasChanged = false;
+
+            for (var i = 0; i < materialPropertyOverride.MaterialOverrides.Count; i++)
+            {
+                var materialOverride = materialPropertyOverride.MaterialOverrides[i];
+
+                EditorGUILayout.Space();
+                renderedHeader = GUILayoutUtility.GetRect(16f, 22f, headerStyle);
+                GUI.Box(renderedHeader, "Material: " + materialOverride.material.name + " (" + materialOverride.material.shader.name + ")",
+                    headerStyle);
+
+                EditorGUILayout.BeginHorizontal();
+
+                EditorGUI.BeginChangeCheck();
+
+                var activeProperty = serializedObject.FindProperty("_materialOverrides.Array.data[" + i + "].isActive");
+                if (activeProperty != null) EditorGUILayout.PropertyField(activeProperty, new GUIContent("Active"));
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    hasChanged = true;
+                    serializedObject.ApplyModifiedProperties();
+                }
+
+                GUILayout.FlexibleSpace();
+
+                if (materialOverride.isActive)
+                {
+                    materialOverride.canShowAll = GUILayout.Toggle(materialOverride.canShowAll, "Show All", "Button", GUILayout.Width(70f));
+                }
+
+                EditorGUILayout.EndHorizontal();
+
+                if (materialOverride.isActive)
+                {
+                    if (Selection.gameObjects.Length > 1)
+                    {
+                        EditorGUILayout.HelpBox("Multi editing not supported", MessageType.Info);
+                    }
+                    else
+                    {
+                        hasChanged |= DrawOverrideGUI(materialOverride.material.shader, materialOverride.propertyOverrides,
+                            materialOverride.canShowAll, materialPropertyOverride);
+                    }
+                }
+            }
+
+            if (!hasChanged) return;
+
+            materialPropertyOverride.ClearMaterialProperties();
+            materialPropertyOverride.ApplyMaterialProperties();
+        }
+
+        private static bool DrawOverrideGUI(
+            Shader shader,
+            List<MaterialPropertyOverride.ShaderPropertyValue> propertyOverrides,
+            bool canShowAll,
+            Object target)
+        {
+            var shaderProperties = GetShaderProperties(shader);
+
+            foreach (var shaderProperty in shaderProperties)
+            {
+                var propertyOverride = propertyOverrides.Find(x => x.propertyName == shaderProperty.property);
+                bool hasPropertyOverride = propertyOverride != null;
+                if (!hasPropertyOverride && !canShowAll) continue;
+
+                GUILayout.BeginHorizontal();
+                var isButtonPressed = false;
+
+                if (canShowAll) isButtonPressed = GUILayout.Button(hasPropertyOverride ? "-" : "+", GUILayout.Width(20f));
+                var description = new GUIContent(shaderProperty.description, shaderProperty.property);
+
+                if (!hasPropertyOverride)
+                {
+                    GUILayout.Label(description);
+                    if (isButtonPressed)
+                    {
+                        var shaderPropertyValue = new MaterialPropertyOverride.ShaderPropertyValue
+                        {
+                            type = shaderProperty.type,
+                            propertyName = shaderProperty.property,
+                        };
+
+                        Undo.RecordObject(target, "Override");
+                        propertyOverrides.Add(shaderPropertyValue);
+                    }
+                }
+                else
+                {
+                    Undo.RecordObject(target, "Override");
+                    switch (shaderProperty.type)
+                    {
+                        case ShaderPropertyType.Color:
+                            propertyOverride.colorValue = EditorGUILayout.ColorField(description, propertyOverride.colorValue, false, true, true);
+                            break;
+                        case ShaderPropertyType.Float:
+                            propertyOverride.floatValue = EditorGUILayout.FloatField(description, propertyOverride.floatValue);
+                            break;
+                        case ShaderPropertyType.Range:
+                            propertyOverride.floatValue = EditorGUILayout.Slider(description, propertyOverride.floatValue, shaderProperty.rangeMin,
+                                shaderProperty.rangeMax);
+                            break;
+                        case ShaderPropertyType.Vector:
+                            propertyOverride.vectorValue = EditorGUILayout.Vector4Field(description, propertyOverride.vectorValue);
+                            break;
+                        case ShaderPropertyType.TexEnv:
+                            propertyOverride.textureValue =
+                                (Texture)EditorGUILayout.ObjectField(description, propertyOverride.textureValue, typeof(Texture), false);
+                            break;
+                        default:
+                            Debug.Log("No property field found");
+                            break;
+                    }
+
+                    if (isButtonPressed) propertyOverrides.Remove(propertyOverride);
+                }
+
+                GUILayout.EndHorizontal();
+            }
+
+            return GUI.changed;
+        }
+    }
+}
---
+++ Assets/_Tools/Scripts/Runtime/Helpers/MaterialPropertyOverride.cs
@@ -0,0 +1,257 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Tools.Helpers
+{
+    public enum ShaderPropertyType
+    {
+        Color = 0,
+        Vector = 1,
+        Float = 2,
+        Range = 3,
+        TexEnv = 4
+    }
+    
+#if UNITY_EDITOR
+    [ExecuteInEditMode]
+#endif
+    public class MaterialPropertyOverride : MonoBehaviour
+    {
+        [Serializable]
+        public class ShaderPropertyValue
+        {
+            public string propertyName;
+            public ShaderPropertyType type;
+            public Color colorValue = Color.white;
+            public float floatValue;
+            public Vector4 vectorValue;
+            public Texture textureValue;
+        }
+        
+        [Serializable]
+        public class MaterialOverride
+        {
+            public bool isActive;
+            [NonSerialized] public bool canShowAll;
+            public Material material;
+            public List<ShaderPropertyValue> propertyOverrides = new();
+            [NonSerialized] public Dictionary<string, ShaderPropertyValue> propertyValueDict = new();
+        }
+
+        #region Variables
+
+        [SerializeField] private List<MaterialOverride> _materialOverrides = new();
+        [SerializeField] private Renderer _renderer;
+
+        private MaterialPropertyBlock _mpb;
+
+        #endregion
+
+        #region Properties
+
+        private MaterialPropertyBlock Mpb => _mpb ??= new MaterialPropertyBlock();
+        public List<MaterialOverride> MaterialOverrides => _materialOverrides;
+        public Renderer Renderer => _renderer;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            foreach (var materialOverride in _materialOverrides)
+            {
+                foreach (var propertyOverride in materialOverride.propertyOverrides)
+                {
+                    materialOverride.propertyValueDict[propertyOverride.propertyName] = propertyOverride;
+                }
+            }
+
+            ApplyMaterialProperties();
+        }
+
+#if UNITY_EDITOR
+        private void OnEnable()
+        {
+            if (Application.isPlaying) return;
+            ApplyMaterialProperties();
+        }
+
+        private void OnDisable()
+        {
+            if (Application.isPlaying) return;
+            ClearMaterialProperties();
+        }
+
+        private void OnValidate()
+        {
+            if (Application.isPlaying) return;
+            ClearMaterialProperties();
+            ApplyMaterialProperties();
+        }
+#endif
+
+        #endregion
+
+        #region Custom Methods
+
+#if UNITY_EDITOR
+        public void FindRenderer() => _renderer = GetComponent<Renderer>();
+#endif
+
+        public void ClearMaterialProperties()
+        {
+            if (!_renderer) return;
+            _renderer.SetPropertyBlock(null);
+            for (int i = 0, c = _renderer.sharedMaterials.Length; i < c; i++)
+            {
+                _renderer.SetPropertyBlock(null, i);
+            }
+        }
+
+        public void ApplyMaterialProperties()
+        {
+            if (!_renderer) return;
+            if (_renderer.sharedMaterials.Length == 0) return;
+
+            var allMatSame = true;
+
+            for (int i = 1, c = _renderer.sharedMaterials.Length; i < c; i++)
+            {
+                if (_renderer.sharedMaterials[i] == _renderer.sharedMaterials[0]) continue;
+
+                allMatSame = false;
+                break;
+            }
+
+            if (allMatSame)
+            {
+                Mpb.Clear();
+                var materialOverride = _materialOverrides.Find(x => x.material == _renderer.sharedMaterials[0]);
+                if (materialOverride == null || materialOverride.isActive == false)
+                {
+                    _renderer.SetPropertyBlock(null);
+                }
+                else
+                {
+                    ApplyOverrides(Mpb, materialOverride.propertyOverrides);
+                    _renderer.SetPropertyBlock(Mpb);
+                }
+            }
+            else
+            {
+                for (int i = 0, c = _renderer.sharedMaterials.Length; i < c; i++)
+                {
+                    var materialOverride = _materialOverrides.Find(x => x.material == _renderer.sharedMaterials[i]);
+                    if (materialOverride == null || materialOverride.isActive == false)
+                    {
+                        _renderer.SetPropertyBlock(null, i);
+                    }
+
+                    else
+                    {
+                        Mpb.Clear();
+                        ApplyOverrides(Mpb, materialOverride.propertyOverrides);
+                        _renderer.SetPropertyBlock(Mpb, i);
+                    }
+                }
+            }
+        }
+
+        private void ApplyOverrides(MaterialPropertyBlock mpb, List<ShaderPropertyValue> shaderPropertyValues)
+        {
+            foreach (var shaderPropertyValue in shaderPropertyValues)
+            {
+                switch (shaderPropertyValue.type)
+                {
+                    case ShaderPropertyType.Color:
+                        mpb.SetColor(shaderPropertyValue.propertyName, shaderPropertyValue.colorValue);
+                        break;
+                    case ShaderPropertyType.Float:
+                    case ShaderPropertyType.Range:
+                        mpb.SetFloat(shaderPropertyValue.propertyName, shaderPropertyValue.floatValue);
+                        break;
+                    case ShaderPropertyType.Vector:
+                        mpb.SetVector(shaderPropertyValue.propertyName, shaderPropertyValue.vectorValue);
+                        break;
+                    case ShaderPropertyType.TexEnv:
+                        if (shaderPropertyValue.textureValue) mpb.SetTexture(shaderPropertyValue.propertyName, shaderPropertyValue.textureValue);
+                        break;
+                    default:
+#if UNITY_EDITOR
+                        Debug.Log("Shader property type not found");
+#endif
+
+                        break;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        public Color GetColorValue(string propertyName, int materialIndex)
+        {
+            return !_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)
+                ? Color.clear
+                : _materialOverrides[materialIndex].propertyValueDict[propertyName].colorValue;
+        }
+
+        public void SetColorProperty(string propertyName, Color color, int materialIndex)
+        {
+            if (!_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)) return;
+
+            _materialOverrides[materialIndex].propertyValueDict[propertyName].colorValue = color;
+            ApplyMaterialProperties();
+        }
+
+        public Vector4 GetVectorValue(string propertyName, int materialIndex)
+        {
+            return !_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)
+                ? Vector4.zero
+                : _materialOverrides[materialIndex].propertyValueDict[propertyName].vectorValue;
+        }
+
+        public void SetVectorProperty(string propertyName, Vector4 vector, int materialIndex)
+        {
+            if (!_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)) return;
+
+            _materialOverrides[materialIndex].propertyValueDict[propertyName].vectorValue = vector;
+            ApplyMaterialProperties();
+        }
+
+        public float GetFloatValue(string propertyName, int materialIndex)
+        {
+            return !_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)
+                ? 0f
+                : _materialOverrides[materialIndex].propertyValueDict[propertyName].floatValue;
+        }
+
+        public void SetFloatProperty(string propertyName, float value, int materialIndex)
+        {
+            if (!_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)) return;
+
+            _materialOverrides[materialIndex].propertyValueDict[propertyName].floatValue = value;
+            ApplyMaterialProperties();
+        }
+
+        public Texture GetTextureValue(string propertyName, int materialIndex)
+        {
+            return !_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)
+                ? null
+                : _materialOverrides[materialIndex].propertyValueDict[propertyName].textureValue;
+        }
+
+        public void SetTextureProperty(string propertyName, Texture texture, int materialIndex)
+        {
+            if (!_materialOverrides[materialIndex].propertyValueDict.ContainsKey(propertyName)) return;
+
+            _materialOverrides[materialIndex].propertyValueDict[propertyName].textureValue = texture;
+            ApplyMaterialProperties();
+        }
+
+        #endregion
+    }
+}
